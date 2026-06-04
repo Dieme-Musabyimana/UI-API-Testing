e19a2e2e08cb178a022fd261bb54095bc4969b18
Dieme-Musabyimana <musabyimanadieme10@gmail.com>
2026-06-04 14:32:25 +0200

    Validate update product with empty name

--- backend-restassured-java/src/test/java/apitests/Product/UpdateProduct.java
+++ backend-restassured-java/src/test/java/apitests/Product/UpdateProduct.java
@@ -5,32 +5,37 @@ import api.routes.Routes;
 import api.services.AuthService;
 import api.services.ProductService;
 import api.utils.Expectations;
-import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import java.util.Map;
-
 import static api.services.ProductService.newProductName;
 
 public class UpdateProduct {
-
+AuthService authService;
+    @BeforeMethod
+    public void setUp() {
+        this.authService = new AuthService();
+    }
 @Test
-
 public void loginAsAdminAndUpdateProduct(){
-    Response response = new ProductService().updateProduct(Routes.UPDATE_PRODUCT, Map.of("name", newProductName), new AuthService().getLoginToken());
-
+    Response response = new ProductService().updateProduct(Routes.UPDATE_PRODUCT, Map.of("name", newProductName), authService.getLoginToken());
     Assert.assertEquals(response.statusCode(), StatusCodes.OK);
     Assert.assertTrue(response.jsonPath().getBoolean("success"));
     Assert.assertEquals(response.jsonPath().getString("message"), Expectations.PRODUCT_UPDATED);
     Assert.assertEquals(response.jsonPath().getString("data.name"), newProductName);
 }
 @Test
-public void updateProducWithoutLogin() {
+public void updateProductWithoutLogin() {
     Response response = new ProductService().updateProduct(Routes.UPDATE_PRODUCT, Map.of("name",newProductName), "");
     Assert.assertEquals(response.statusCode(), StatusCodes.UNAUTHORIZED);
     Assert.assertEquals(response.jsonPath().getString("message"), Expectations.AUTHENTICATION_ERROR);
+}
+@Test
+    public void updateProductWithEmptyName(){
+    Response response = new ProductService().updateProduct(Routes.UPDATE_PRODUCT, Map.of("name", " "), authService.getLoginToken());
+    Assert.assertEquals(response.statusCode(), StatusCodes.BAD_REQUEST);
 }
 }
 
