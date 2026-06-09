1378e1a4da946b0b3b626676901b4c5be62a1d35
Dieme-Musabyimana <musabyimanadieme10@gmail.com>
2026-06-09 11:05:06 +0200

    Add more validations

--- backend-restassured-java/src/test/java/apitests/CartAPI/GetCurrerntCart.java
+++ backend-restassured-java/src/test/java/apitests/CartAPI/GetCurrerntCart.java
@@ -9,10 +9,33 @@ import org.testng.annotations.Test;
 public class GetCurrerntCart {
 
     @Test
+    public void clearEntireAndGetCurrentCart(){
+        CartService cartService = new CartService();
+        cartService.clearEntireCart();
+        Response response = cartService.getCurrentCart();
 
-    public void getCurrentCartTest(){
-        Response response = new CartService().getCurrentCart();
         Assert.assertEquals(response.statusCode(), StatusCodes.OK);
         Assert.assertTrue(response.jsonPath().getBoolean("success"));
+        Assert.assertEquals(response.jsonPath().getString("message"), "Success");
+        Assert.assertTrue(response.jsonPath().getList("data.items").isEmpty());
+        Assert.assertEquals(response.jsonPath().getInt("data.subtotal"), 0);
+        Assert.assertEquals(response.jsonPath().getInt("data.discount"), 0);
+        Assert.assertEquals(response.jsonPath().getInt("data.total"), 0);
+        Assert.assertEquals(response.jsonPath().getInt("data.itemCount"), 0);
+    }
+
+    @Test
+
+    public void addAndGetCurrentCartTest(){
+        CartService cartService = new CartService();
+        cartService.addToCart();
+        Response response = cartService.getCurrentCart();
+        Assert.assertEquals(response.statusCode(), StatusCodes.OK);
+        Assert.assertTrue(response.jsonPath().getBoolean("success"));
+        Assert.assertFalse(response.jsonPath().getList("data.items").isEmpty());
+        Assert.assertTrue(response.jsonPath().getInt("data.subtotal") > 0);
+        Assert.assertTrue(response.jsonPath().getInt("data.discount") > 0);
+        Assert.assertTrue(response.jsonPath().getInt("data.total") > 0);
+        Assert.assertTrue(response.jsonPath().getInt("data.itemCount") > 0);
     }
 }
